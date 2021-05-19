0863325726ad5fb7d3bdab65dded78418768ad01
Anatoly Khalak <scsi123@mail.ru>
2021-05-19 23:48:50 +0300

    Изменения привязки проекта репозитория. Проект отвязан от репозитория Гит, в среде разработки.

--- sandbox/src/main/java/ru/stqa/pfr/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pfr/sandbox/MyFirstProgram.java
@@ -2,10 +2,10 @@ package ru.stqa.pfr.sandbox;
 
 public class MyFirstProgram {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 
-	System.out.println("Hello, world!");
+    System.out.println("Hello, world!");
 
-	}
+  }
 
 }
